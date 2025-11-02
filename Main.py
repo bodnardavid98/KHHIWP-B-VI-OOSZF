f4c636445f3ec48638424fc84a34262f24af0d49
bodnardavid98 <bodnar.david.98@gmail.com>
2025-11-02 20:42:50 +0100

    Fill rental with cars, full control loop

--- Main.py
+++ Main.py
@@ -2,11 +2,10 @@ from PersonalVehicle import PersonalVehicle
 from Truck import Truck
 from Rental import Rental
 
-auto = PersonalVehicle("asd-123", 20000, 2)
-auto = Truck("asd-123", 20000, 20)
-print(auto.Type)
-
 rental = Rental("Car rental company")
+rental.addCar(Truck("ASD-123", 20000, 20))
+rental.addCar(PersonalVehicle("QWE-456", 10000, 2))
+rental.addCar(PersonalVehicle("YXC-789", 15000, 4))
 
 while True:
     print("Válaszd ki a funkciót.\n 1 Autó bérlése\n 2 Bérlés lemondása\n 3 Bérlések listázása\n 4 Kilépés")
@@ -14,7 +13,11 @@ while True:
     if Function != "1" and Function != "2" and Function != "3" and Function != "4":
         print("Ez nem egy opció")
     else:
-        if Function == "3":
+        if Function == "1":
+            rental.bookRent()
+        elif Function == "2":
+            rental.backRent()
+        elif Function == "3":
             rental.printRents()
-        if Function == "4":
+        elif Function == "4":
             break
